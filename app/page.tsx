ca71bdaebddf6947f237ea134cee55a645bbeac3
Onlook <git@onlook.com>
2025-05-02 22:41:15 +0530

    narrow the secion

--- app/page.tsx
+++ app/page.tsx
@@ -371,51 +371,105 @@ export default function Page() {
                                     NOTIFY X
                                 </a>
                             </div>
-                            <div className="flex items-center space-x-2 mt-0" data-oid="tq.vs1d">
-                                <span
-                                    className="text-xs font-medium text-gray-600"
-                                    data-oid="gt6p2._"
+                            <div
+                                className="flex items-center space-x-3 mt-2 sm:mt-0"
+                                data-oid="tq.vs1d"
+                            >
+                                {/* Mobile view - compact icons */}
+                                <div
+                                    className="flex sm:hidden items-center space-x-2"
+                                    data-oid="pv6qm:p"
                                 >
-                                    Join:
-                                </span>
-                                <a
-                                    href="/telegram"
-                                    className="inline-flex items-center justify-center w-7 h-7 bg-teal-500 hover:bg-teal-600 text-white rounded-full transition-all duration-300"
-                                    title="Join Telegram Community"
-                                    data-oid="ufxxan3"
-                                >
-                                    <svg
-                                        className="h-4 w-4"
-                                        fill="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        data-oid="p:ihg8i"
+                                    <span
+                                        className="text-xs font-medium text-gray-600"
+                                        data-oid="11sze37"
                                     >
-                                        <path
-                                            d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.96 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"
-                                            data-oid="my5tivf"
-                                        ></path>
-                                    </svg>
-                                </a>
-                                <a
-                                    href="/whatsapp"
-                                    className="inline-flex items-center justify-center w-7 h-7 bg-green-500 hover:bg-green-600 text-white rounded-full transition-all duration-300"
-                                    title="Join WhatsApp Community"
-                                    data-oid="ubl2i49"
-                                >
-                                    <svg
-                                        className="h-4 w-4"
-                                        fill="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        data-oid="7ohq35_"
+                                        Join:
+                                    </span>
+                                    <a
+                                        href="/telegram"
+                                        className="inline-flex items-center justify-center w-7 h-7 bg-teal-500 hover:bg-teal-600 text-white rounded-full transition-all duration-300"
+                                        title="Join Telegram Community"
+                                        data-oid="::kzzpf"
                                     >
-                                        <path
-                                            d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"
-                                            data-oid="17d5xvp"
-                                        ></path>
-                                    </svg>
-                                </a>
+                                        <svg
+                                            className="h-4 w-4"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            data-oid="hizck6k"
+                                        >
+                                            <path
+                                                d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.96 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"
+                                                data-oid="r8o.rd4"
+                                            ></path>
+                                        </svg>
+                                    </a>
+                                    <a
+                                        href="/whatsapp"
+                                        className="inline-flex items-center justify-center w-7 h-7 bg-green-500 hover:bg-green-600 text-white rounded-full transition-all duration-300"
+                                        title="Join WhatsApp Community"
+                                        data-oid="poe912i"
+                                    >
+                                        <svg
+                                            className="h-4 w-4"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            data-oid="41pg8ol"
+                                        >
+                                            <path
+                                                d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"
+                                                data-oid="xzw0bqw"
+                                            ></path>
+                                        </svg>
+                                    </a>
+                                </div>
+
+                                {/* Desktop view - original buttons */}
+                                <div
+                                    className="hidden sm:flex items-center space-x-3"
+                                    data-oid="gy2e2t-"
+                                >
+                                    <a
+                                        href="/telegram"
+                                        className="inline-flex items-center px-3 py-1 bg-teal-500 hover:bg-teal-600 text-white text-xs font-medium rounded-md transition-all duration-300"
+                                        data-oid="ufxxan3"
+                                    >
+                                        <svg
+                                            className="h-4 w-4 mr-1"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            data-oid="p:ihg8i"
+                                        >
+                                            <path
+                                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.1 14.9l-3.2-3.2c-.2-.2-.2-.5 0-.7l.7-.7c.2-.2.5-.2.7 0l2.1 2.1 5.6-5.6c.2-.2.5-.2.7 0l.7.7c.2.2.2.5 0 .7l-6.6 6.6c-.2.2-.5.2-.7.1z"
+                                                data-oid="vlc22re"
+                                            ></path>
+                                        </svg>
+                                        Join Telegram Community
+                                    </a>
+                                    <a
+                                        href="/whatsapp"
+                                        className="inline-flex items-center px-3 py-1 bg-green-500 hover:bg-green-600 text-white text-xs font-medium rounded-md transition-all duration-300"
+                                        data-oid="ubl2i49"
+                                    >
+                                        <svg
+                                            className="h-4 w-4 mr-1"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            data-oid="7ohq35_"
+                                        >
+                                            <path
+                                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.1 14.9l-3.2-3.2c-.2-.2-.2-.5 0-.7l.7-.7c.2-.2.5-.2.7 0l2.1 2.1 5.6-5.6c.2-.2.5-.2.7 0l.7.7c.2.2.2.5 0 .7l-6.6 6.6c-.2.2-.5.2-.7.1z"
+                                                data-oid="f_rzdk8"
+                                            ></path>
+                                        </svg>
+                                        Join WhatsApp Community
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
